3d91a4cd54a93bc1b1bfaab318eb259307f15819
joe.dakroub <joe.dakroub@me.com>
2025-10-31 10:09:22 -0700

    Bump

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  base: "/filters/",
+  base: "/app-filters/",
   server: {
     https: true,
     host: "0.0.0.0",
